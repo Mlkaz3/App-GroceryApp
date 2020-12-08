4a23127919e2ca3130cc5477bd847f39b2f045d3
Andrew <andrewty587@gmail.com>
2020-12-08 17:13:59 +0800

    add Recipe.kt complete UI and fuction of Recipe except the connection to vegan and non vegan registration complete with php login incomplete of php
    done registration and login with php
    
    added get user id and cart id

--- app/src/main/java/com/example/groceryapp/LoginActivity.kt
+++ app/src/main/java/com/example/groceryapp/LoginActivity.kt
@@ -3,21 +3,14 @@ package com.example.groceryapp
 import android.app.ProgressDialog
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.AuthFailureError
-import com.android.volley.DefaultRetryPolicy
-import com.android.volley.Request
 import com.android.volley.Response
-import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
-import com.example.groceryapp.Adapter.MySingleton
-import org.json.JSONArray
-import org.json.JSONObject
 import java.util.*
 
 
@@ -50,62 +43,7 @@ class LoginActivity : AppCompatActivity() {
                 if (response.equals("logged in successfully", ignoreCase = true)) {
                     ed_email!!.setText("")
                     ed_password!!.setText("")
-
-
-                    //read the user user_id and cart_id from the server
-                    val url = "https://groceryapptarucproject.000webhostapp.com/grocery/getdata.php?email=" + str_email + "&password=" + str_password
-                    val jsonObjectRequest = JsonObjectRequest(
-                        Request.Method.GET, url, null,
-                        Response.Listener { response ->
-                            var userID = ""
-                            var cartID = ""
-
-                            Log.e("response",response.toString())
-                            // Process the JSON
-                            try{
-                                if(response != null){
-                                    val strResponse = response.toString()
-                                    val jsonResponse  = JSONObject(strResponse)
-                                    val jsonArray: JSONArray = jsonResponse.getJSONArray("userinfo")
-                                    val size: Int = jsonArray.length()
-                                    for(i in 0.until(size)){
-                                        var jsonCartItem: JSONObject = jsonArray.getJSONObject(i)
-
-                                        userID = jsonCartItem.getString("user_id")
-                                        cartID = jsonCartItem.getString("cart_id")
-                                        Log.e("get userID",userID)
-                                        Log.e("get cartID",cartID)
-
-
-                                        //pass the user cart_id to next activity
-                                        var user_id:Int = userID.toInt()
-                                        var cart_id:Int = cartID.toInt()
-                                        val intent = Intent(baseContext, MainActivity::class.java)
-                                        intent.putExtra("cart_id", cart_id.toString())
-                                        intent.putExtra("user_id",user_id.toString())
-                                        startActivity(intent)
-
-                                    }
-                                }
-                            }catch (e:Exception){
-                                Log.d("Main", "Response: %s".format(e.message.toString()))
-                            }
-                        },
-                        Response.ErrorListener { error ->
-                            Log.d("Main", "Response: %s".format(error.message.toString()))
-                        }
-                    )
-
-                    //Volley request policy, only one time request
-                    jsonObjectRequest.retryPolicy = DefaultRetryPolicy(
-                        DefaultRetryPolicy.DEFAULT_TIMEOUT_MS,
-                        0, //no retry
-                        1f
-                    )
-                    // Access the RequestQueue through your singleton class.
-                    MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
-
-
+                    startActivity(Intent(this, MainActivity::class.java))
                     Toast.makeText(this@LoginActivity, response, Toast.LENGTH_SHORT).show()
                 } else {
                     Toast.makeText(this@LoginActivity, response, Toast.LENGTH_SHORT).show()
---
+++ app/src/main/java/com/example/groceryapp/User.kt
@@ -0,0 +1,4 @@
+package com.example.groceryapp
+
+
+class User(val user_id: Int, val name: String,val cart_id:Int)
